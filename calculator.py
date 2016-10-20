0e79efdef1893dce197db70b91645cb51942b96c
Marcell Lovácsi <m.lovacsi@gmail.com>
2016-10-20 11:07:04 +0200

    calc v1.1 sqrt added

--- calculator.py
+++ calculator.py
@@ -1,17 +1,28 @@
+import math
+print("\nWelcome to the calculator script!\n")
 while True:
     try:
-        num1 = int(input("Enter a number or a letter to exit "))
-        operator = input("Enter an operator ")
-        num2 = int(input("Enter another number "))
+        def inputNum2():
+            global num2
+            num2 = int(input("Enter another number: "))
+        num1 = int(input("Enter a number or a letter to exit: "))
+        operator = input("Enter an operator: ")
         if operator == "+":
+            inputNum2()
             print("{} + {} = {}".format(num1, num2, num1+num2))
         elif operator == "-":
+            inputNum2()
             print("{} - {} = {}".format(num1, num2, num1-num2))
         elif operator == "*":
+            inputNum2()
             print("{} * {} = {}".format(num1, num2, num1*num2))
         elif operator == "/":
+            inputNum2()
             print("{} / {} = {}".format(num1, num2, num1/num2))
+        elif operator == "sqrt":
+            print("{} √ = {}".format(num1, math.sqrt(num1)))
         else:
-            print("Use +,-,* or / next time!")
+            print("Use +,-,*,/ or sqrt next time!\n")
     except ValueError:
+        print("\nThanks for using this calculator!\n")
         break
