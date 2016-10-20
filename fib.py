72bd1e678660cd321e5c123bbb72bc59ba42f866
Marcell Lovácsi <m.lovacsi@gmail.com>
2016-10-20 17:37:51 +0200

    v1.1 Added dynamic spaces

--- fib.py
+++ fib.py
@@ -1,6 +1,7 @@
 import math
 seqNum=int(input("How many numbers would you like to see?"))
 print("Fibonacci sequence for {} numbers: ".format(seqNum))
+spaces=40
 sorszam=0
 num1=0
 num2=1
@@ -10,4 +11,11 @@ for i in range(seqNum):
     fib=num1+num2
     num1=num2
     num2=fib
-    print("{}. {}".format(sorszam,num1))
+    sorszamstring=str(sorszam)
+    sorszamstringNum=len(sorszamstring)
+    notspaces = str(fib)
+    notspacNum=len(notspaces)
+    print("{}.".format(sorszam), end="")
+    for i in range((spaces - notspacNum) - sorszamstringNum):
+        print(" ", end="")
+    print("{}".format(fib))
