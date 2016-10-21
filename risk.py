1f78a352fa72f26aac74758c666f867471ce5e9b
Marcell Lovácsi <m.lovacsi@gmail.com>
2016-10-21 10:40:51 +0200

    Initial commit for risk game

---
+++ risk.py
@@ -0,0 +1,6 @@
+import random
+attackNum1 = random.randrange(1:7)
+attackNum2 = random.randrange(1:7)
+defendNum1 = random.randrange(1:7)
+defendNum2 = random.randrange(1:7)
+print("Attacker {} -- {} Defender\nAttacker {} -- {} Defender".format(attackNum1,defendNum1,attackNum2,defendNum2))
