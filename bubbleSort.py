93d2fb8b640700e93f0272e72f44bb3fbee55b16
Marcell Lovácsi <m.lovacsi@gmail.com>
2016-10-26 09:30:50 +0200

    bubblesort made easier courtesy of ákos

--- bubbleSort.py
+++ bubbleSort.py
@@ -5,8 +5,8 @@ for i in range(0,10):
 for i in range(0,10):
     for o in range(0,9):
         if szamok[o+1] < szamok[o]:
-            temp = szamok[o]
+            """temp = szamok[o]
             szamok[o] = szamok[o+1]
-            szamok[o+1] = temp
-
+            szamok[o+1] = temp"""
+            szamok[o], szamok[o+1] = szamok[o+1], szamok[o]
 print(szamok)
